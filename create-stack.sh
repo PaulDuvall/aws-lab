69f62c7b02caa3f5da073a822321543afd791c4d
Mark S. Maglana <mmaglana@gmail.com>
2020-12-05 15:45:59 +0800

    Simplify stack for now by removing custom VPC

--- create-stack.sh
+++ create-stack.sh
@@ -6,9 +6,7 @@ set -x
 aws cloudformation create-stack \
     --stack-name ${stack_name} \
     --template-body file://./vpc.yml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameters \
-        ParameterKey=EnvironmentName,ParameterValue=${envname}
+    --capabilities CAPABILITY_NAMED_IAM
 
 { set +x; } 2>/dev/null
 echo "Track progress at https://console.aws.amazon.com/cloudformation"
