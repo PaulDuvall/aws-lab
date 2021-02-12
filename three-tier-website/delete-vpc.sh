9d100ccfef5dd594b38f03dd1cfbb6d096aaca9f
Paul Duvall <pduvall@gmail.com>
2021-02-12 20:57:32 +0000

    changes

--- three-tier-website/delete-vpc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -e
-envname=${1:-prod}
-stack_name=lab-${envname}
-set -x
-aws cloudformation delete-stack --stack-name ${stack_name} --region ap-southeast-1
-{ set +x; } 2>/dev/null
-echo "Track progress at https://console.aws.amazon.com/cloudformation"
-aws cloudformation wait stack-delete-complete --stack-name ${stack_name} --region ap-southeast-1
